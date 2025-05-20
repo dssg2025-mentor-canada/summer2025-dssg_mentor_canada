4cd1183d4964b240852d1187077bcb6c4255a3aa
yvinc <imvincy@outlook.com>
2025-05-20 14:52:33 -0700

    changing relative path in eda.py

--- eda.py
+++ eda.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('/Users/isobeladams/Desktop/data.csv')
+df = pd.read_csv('../../dssg-2025-mentor-canada/Data/Data_2020-Youth-Survey.csv')
 
 print(df.head())
 print(df.info())
