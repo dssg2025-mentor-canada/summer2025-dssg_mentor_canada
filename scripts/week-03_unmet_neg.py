6d6b1f24029251b0505bc6883e50458a4fcb4717
maxineparedes <maxineparedes@gmail.com>
2025-06-10 14:47:18 -0700

    clean up script

--- scripts/week-03_unmet_neg.py
+++ scripts/week-03_unmet_neg.py
@@ -8,13 +8,6 @@ predictor_cols = ["38_social_assistance",
                   "38_food_bank_use"]
 target_cols = ["18d_early_mentor_unmet_access"]
 
-# # recode 18d_early_mentor_unmet_access to be binary
-# logistic_data = df[
-#     df["38_social_assistance"].isin([1, 2]) &
-#     df["38_work_to_support_family"].isin([1, 2]) &
-#     df["38_food_bank_use"].isin([1, 2])
-# ].copy()
-
 logistic_data = df.copy()
 
 logistic_data['18d_early_mentor_unmet_access'] = (
@@ -43,28 +36,6 @@ logistic_data[target_cols] = (
     .astype('Int64')
 )
 
-# logistic_data['38_food_bank_use'] = (
-#     logistic_data['38_food_bank_use']
-#     .map({2: 0})
-#     .astype('Int64')
-# )
-
-
-# # keep only observations that had yes or no answers
-# valid_rows = logistic_data[[
-#     "38_social_assistance", 
-#     "38_work_to_support_family", 
-#     "38_food_bank_use",
-#     "18d_early_mentor_unmet_access"
-# ]].isin([1, 0]).all(axis=1)
-
-
-# # filter data
-# logistic_data = logistic_data[valid_rows]
-
-# # # # keep only observations that had yes or no answers
-# # logistic_data = df['18d_early_mentor_unmet_access'].isin([1,2])
-
 # check for class imbalance -> if so, reweight
 logistic_data['18d_early_mentor_unmet_access'].value_counts()
 
@@ -75,11 +46,3 @@ y = logistic_data['18d_early_mentor_unmet_access'].astype('float64')
 model = sm.Logit(y, X).fit()
 
 print(model.summary())
-
-# print(X.dtypes)
-# print(y.dtypes)
-
-# print(logistic_data[["38_social_assistance",
-#                   "38_work_to_support_family",
-#                   "38_food_bank_use",
-#                   "18d_early_mentor_unmet_access"]])
