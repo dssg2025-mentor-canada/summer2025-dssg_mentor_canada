f31af4c41549323b8b9ee73a70d6cadf1443e8cc
maxineparedes <maxineparedes@gmail.com>
2025-06-10 11:08:35 -0700

    need to fix unmet script

--- scripts/week-03_unmet_neg.py
+++ scripts/week-03_unmet_neg.py
@@ -13,9 +13,28 @@ logistic_data = df[['18d_early_mentor_unmet_access',
 
 logistic_data['18d_early_mentor_unmet_access'] = (
     logistic_data['18d_early_mentor_unmet_access']
-    .map({'Yes': 1, 'No': 2})
+    .map({'Yes': 1, 'No': 0})
     .astype('Int64')
 )
+
+logistic_data['38_social_assistance'] = (
+    logistic_data['38_social_assistance']
+    .map({2: 0})
+    .astype('Int64')
+)
+
+logistic_data['38_work_to_support_family'] = (
+    logistic_data['38_work_to_support_family']
+    .map({2: 0})
+    .astype('Int64')
+)
+
+logistic_data['38_food_bank_use'] = (
+    logistic_data['38_food_bank_use']
+    .map({2: 0})
+    .astype('Int64')
+)
+
 
 # keep only observations that had yes or no answers
 valid_rows = logistic_data[[
@@ -23,22 +42,19 @@ valid_rows = logistic_data[[
     "38_work_to_support_family", 
     "38_food_bank_use",
     "18d_early_mentor_unmet_access"
-]].isin([1, 2]).all(axis=1)
+]].isin([1, 0]).all(axis=1)
 
 
 # filter data
 logistic_data = logistic_data[valid_rows]
-print(logistic_data['18d_early_mentor_unmet_access'].unique())
-
-# print(logistic_data['18d_early_mentor_unmet_access'].unique())
 
 # # keep only observations that had yes or no answers
-# logistic_data = df['18d_early_mentor_unmet_access'].isin([1,2])
+logistic_data = df['18d_early_mentor_unmet_access'].isin([1,2])
 
-# X = df[predictor_cols]
-# X = sm.add_constant(X)
-# y = df['18d_early_mentor_unmet_access']
+X = logistic_data[predictor_cols]
+X = sm.add_constant(X)
+y = logistic_data['18d_early_mentor_unmet_access'].astype(float)
 
-# model = sm.Logit(y, X).fit()
+model = sm.Logit(y, X).fit()
 
-# print(model.summary())
+print(model.summary())
