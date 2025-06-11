6459ba24047f24ac904b0daa561a93b9ce90227c
maxineparedes <maxineparedes@gmail.com>
2025-06-11 14:19:51 -0700

    logistic regression of low SES and teen unmet needs

---
+++ scripts/week-04_unmet_need_teen.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import statsmodels.api as sm
+
+df = pd.read_csv("../../dssg-2025-mentor-canada/Data/youth_tidy.csv")
+
+predictor_cols = ["38_social_assistance",
+                  "38_work_to_support_family",
+                  "38_food_bank_use"]
+target_cols = ["19c_teen_mentor_unmet_access"]
+
+print('count of those with guardians that required social assistance during youth (ages 12–18):', (df['38_social_assistance'] == 1).sum())
+print('count of those who needed to work to support their families during youth (ages 12-18):', (df['38_work_to_support_family'] == 1).sum())
+print('count of those who used food banks during youth (ages 12-18):', (df['38_food_bank_use'] == 1).sum())
+print("count of those who wanted a mentor but had unmet access as a teen (ages 12-18):",
+      (df['19c_teen_mentor_unmet_access']== 'Yes').sum())
+print("\nRaw target class distributions:")
+
+print(df['19c_teen_mentor_unmet_access'].value_counts())
+
+logistic_data = df.copy()
+
+logistic_data['19c_teen_mentor_unmet_access'] = (
+    logistic_data['19c_teen_mentor_unmet_access']
+    .map({'Yes': 1, 'No': 2})
+    .astype('Int64')
+)
+
+# recode 19c_teen_mentor_unmet_access to be binary
+logistic_data = logistic_data[
+    logistic_data["38_social_assistance"].isin([1, 2]) &
+    logistic_data["38_work_to_support_family"].isin([1, 2]) &
+    logistic_data["38_food_bank_use"].isin([1, 2]) &
+    logistic_data["19c_teen_mentor_unmet_access"].isin([1, 2])
+].copy()
+
+logistic_data[predictor_cols] = (
+    logistic_data[predictor_cols]
+    .replace({2: 0})
+    .astype('Int64')
+)
+
+logistic_data[target_cols] = (
+    logistic_data[target_cols]
+    .replace({2: 0})
+    .astype('Int64')
+)
+
+# check for class imbalance -> if so, reweight
+logistic_data['19c_teen_mentor_unmet_access'].value_counts()
+
+X = logistic_data[predictor_cols].astype('float64')
+X = sm.add_constant(X)
+y = logistic_data['19c_teen_mentor_unmet_access'].astype('float64')
+
+model = sm.Logit(y, X).fit()
+
+print(model.summary())
