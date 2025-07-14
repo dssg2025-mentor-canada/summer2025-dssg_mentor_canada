d951102b488d28bea41b74a8cafce042deb37832
maxineparedes <maxineparedes@gmail.com>
2025-07-14 10:43:14 -0700

    converted current pdfs into images

--- rag/scripts/apply_ocr.py
+++ rag/scripts/apply_ocr.py
@@ -8,7 +8,7 @@ from pdf2image import convert_from_path
 from glob import glob
 
 # convert pdf to png
-def pdf_to_image(pdf_path, output_folder= "../rag/pdf_images"):
+def pdf_to_image(pdf_path, output_folder="rag/pdf_images"):
     """
     function to convert pdfs to images
     """
@@ -21,10 +21,20 @@ def pdf_to_image(pdf_path, output_folder= "../rag/pdf_images"):
     return pages
 
 # convert all existing items in the folder
-ALL_PDF_PATHS = glob("../rag/raw_pdfs")
+ALL_PDF_PATHS = glob("rag/raw_pdfs/*.pdf")
+
+from pdf2image.exceptions import PDFPageCountError
 
 for pdf_file in ALL_PDF_PATHS:
-    print(f"converting {pdf_file} into .png file")
-    pdf_to_image(pdf_path=pdf_file, output="../rag/pdf_images")
+    # print(f"converting {pdf_file} into .png file")
+    # pdf_to_image(pdf_path=pdf_file, output_folder="rag/pdf_images")
+    try:
+        print(f"Processing: {pdf_file}")
+        pdf_to_image(pdf_path=pdf_file, output_folder="rag/pdf_images")
+    except PDFPageCountError:
+        print(f"❌ Skipping unreadable PDF: {pdf_file}")
+    except Exception as e:
+        print(f"❌ Unexpected error with {pdf_file}: {e}")
 
-print(f"Successfully converted {len(ALL_PDF_PATHS)} files :)")
+# print(f"Successfully converted {len(ALL_PDF_PATHS)} files :)")
+print(ALL_PDF_PATHS)
