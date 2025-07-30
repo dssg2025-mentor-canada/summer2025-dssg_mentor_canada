0345f7b02dc09f62227bcd1c54b69972a9aeadfb
maxineparedes <maxineparedes@gmail.com>
2025-07-30 14:15:32 -0700

    editing prompt template (in progress)

--- rag/scripts/query_data_pc.py
+++ rag/scripts/query_data_pc.py
@@ -9,11 +9,22 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
 from get_embedding_function import get_embedding_function
 
-CHROMA_PATH = "rag/chroma"
-
-
 PROMPT_TEMPLATE = """
-You are a domain expert in mentorship. Answer the question based only on the following context:
+You are a domain expert in mentorship. The context that you are receiving is extracted from multiple reports detailing mentorship findings,
+information, and statistical insights that have all been published by Mentor Canada. Because you are pulling from multiple sources that may be
+disjoint, ensure that you are attempting to specify the situation as much as possible. For example, if asked about what are good qualities of
+a mentor, and if you come across a chunk that addresses this question specific to newcomer youth, include this context (e.g., "For 
+newcomer youth, a good mentor may ..."). However, do not focus the response on one group unless specifically asked to do so.
+
+Please add the sources of the documents in the database from which you construct the answer. For example i want this indicated as subscripted 
+numbers through the text response in addition to these numbers and sources are indicated at the bottom of the response. In the references section,
+it should only be a number accompanied by the name of the orginal pdf that this chunk came from.
+
+Produce a response that is conversational in tone but maintains the level of nuance needed when delivering information that may be sensitive. 
+Responses should avoid run on sentences and listing things extensively. The flow of the response should be smooth and while maintaining a high 
+level or detail.
+
+Answer the question based only on the following context:
 
 {context}
 
